ddcdecf07ceb0fc3d495d0b12563ee229d8d2591
Wenzheng Feng <wenzheng.feng@outlook.com>
2022-08-17 10:22:25 -0700

    unit test working

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstraintCTDateTest()
+        {
+
+            //arrange
+            //var ctDate = DateTime.Now.AddDays(-59);
+            var ctDate = DateTime.Now.AddDays(-61);
+
+            // act
+            var constraint = new CTDateConstraint();
+            var actual = constraint.ConstraintCTDate(ctDate).ResultType;
+
+            //assert
+            //var expected = ResultType.PASSED;
+            var expected = ResultType.ACTION_LEVEL_3;
+            Assert.AreEqual(expected, actual);
+            //Assert.Fail();
+        }
+    }
+}
